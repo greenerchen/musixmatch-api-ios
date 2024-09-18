f2746196023e91a3ca06afc5efc0ec6360065d9c
Greener Chen <greener@gmail.com>
2024-09-18 17:39:45 +0800

    Separate MusixmatchAPISessionMock to a new helper file

--- Tests/MusixmatchAPITests/MusixmatchAPIClientTests.swift
+++ Tests/MusixmatchAPITests/MusixmatchAPIClientTests.swift
@@ -117,20 +117,3 @@ final class MusixmatchAPIClientTests: XCTestCase {
         XCTAssertEqual(lyrics.body, "Heart beats fast", "lyricsBody: \(lyrics.body)")
     }
 }
-
-final class MusixmatchAPISessionMock: URLSessionProtocol {
-    
-    var getUrlCallCount: Int = 0
-    
-    var getUrlResultStub: (data: Data, response: URLResponse)
-    
-    init(getUrlResultStub: (data: Data, response: URLResponse)) {
-        self.getUrlResultStub = getUrlResultStub
-    }
-    
-    func get(_ url: URL) async throws -> (data: Data, response: URLResponse) {
-        getUrlCallCount += 1
-        return getUrlResultStub
-    }
-}
-
